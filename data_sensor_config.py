95ed8e40ba0b3e0572f51c62e9f75a384cd33dcf
Thermo-TADAR <152589282+Thermo-TADAR@users.noreply.github.com>
2023-12-09 19:46:27 +0800

    Delete data_sensor_config.py

--- data_sensor_config.py
+++
@@ -1,9 +0,0 @@
-sensors_info_list = [
-    {"topic": "/s1nup/tx", "device_type": 1 },
-    {"topic": "/s0wup/tx", "device_type": 0 },
-    {"topic": "/s0nup/tx", "device_type": 0 },
-
-    {"topic": "/s1ndown/tx", "device_type": 1 },
-    {"topic": "/s0wdown/tx", "device_type": 0 },
-    {"topic": "/s0ndown/tx", "device_type": 0 },
-    ]
